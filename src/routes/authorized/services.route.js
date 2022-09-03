8dc4eb6fd27a33fd8cc3529a885a71997fd9a0af
Jamshaid Sabir <jamshaidsabir411980@gmail.com>
2022-09-03 10:53:37 +0500

    Minor change

--- src/routes/authorized/services.route.js
+++ src/routes/authorized/services.route.js
@@ -7,14 +7,14 @@ const router = express.Router()
 var permit = require('../../middlewares').permit
 const controller = require('../../controllers').services
 
-router.post('/createService', controller.createService)
+
 
 router.post('/updateService', controller.updateService)
 router.post('/approveDisapproveService', permit(['_a']), controller.approveDisapproveService)
 router.post('/removeService', controller.removeService)
 router.post('/getServicesWithFullDetails', controller.getServicesWithFullDetails)
 router.post('/getServicesList', controller.getServicesList)
-router.post('/findServiceById', controller.findServiceById)
+
 router.post('/populateDBWithSrvsPrvs', controller.populateDBWithSrvsPrvs)
 router.post('/populateDBWithDoctorsPrvs', controller.populateDBWithDoctorsPrvs)
 router.post('/populateDBWithLawyersPrvs', controller.populateDBWithLawyersPrvs)

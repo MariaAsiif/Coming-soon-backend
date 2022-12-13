6df294a62bd1f265fabd43d6fc13f9f548d69784
jamshaid411980 <33445322+jamshaid411980@users.noreply.github.com>
2022-12-13 21:58:08 +0500

    Final deployment 2

--- src/routes/authorized/productsOfStore.route.js
+++ src/routes/authorized/productsOfStore.route.js
@@ -8,12 +8,12 @@ const permit = require('../../middlewares').permit
 
 const controller = require('../../controllers').productsOfStore
 
-router.post('/createProductsOfStore', permit(['_a']), controller.createProductsOfStore)
-router.post('/getProductsOfStoresWithFullDetails', permit(['_a']), controller.getProductsOfStoresWithFullDetails)
-router.post('/updateProductsOfStore', permit(['_a']), controller.updateProductsOfStore)
-router.post('/removeProductsOfStore', permit(['_a']), controller.removeProductsOfStore)
-router.post('/getProductsOfStoresList', permit(['_a']), controller.getProductsOfStoresList)
-router.post('/findProductsOfStoreById', permit(['_a']), controller.findProductsOfStoreById)
+router.post('/createProductsOfStore', controller.createProductsOfStore)
+router.post('/getProductsOfStoresWithFullDetails', controller.getProductsOfStoresWithFullDetails)
+router.post('/updateProductsOfStore', controller.updateProductsOfStore)
+router.post('/removeProductsOfStore', controller.removeProductsOfStore)
+router.post('/getProductsOfStoresList', controller.getProductsOfStoresList)
+router.post('/findProductsOfStoreById', controller.findProductsOfStoreById)
 
 
 module.exports = router
